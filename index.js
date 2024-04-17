60b9c9cd0867045f14fecf7121575691cdd8cb48
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-04-16 22:44:22 -0300

    promesas

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ function tres() {
   console.log(`🚀 ~ tres ~ tres:`);
 }
 function cuatro() {
-  return fetch("https://jsonplacehold.typicode.com/todos/1");
+  return fetch("https://jsonplaceholder.typicode.com/todos/1");
+}
+
+async function cinco() {
+  try {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const dataJson = await data.json();
+    console.log(`🚀 ~ cinco ~ data:`, dataJson);
+    return dataJson;
+  } catch (error) {
+    console.log(`🚀 ~ cinco ~ error:`, error);
+  }
 }
 
 uno();
@@ -25,16 +36,19 @@ dos(2)
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
 
-setTimeout(() => {
-  console.log(`🚀 ~ setTimeout ~ setTimeout 1`);
-}, 2000);
-setTimeout(() => {
-  console.log(`🚀 ~ setTimeout ~ setTimeout 2`);
-});
+// setTimeout(() => {
+//   console.log(`🚀 ~ setTimeout ~ setTimeout 1`);
+// }, 2000);
+// setTimeout(() => {
+//   console.log(`🚀 ~ setTimeout ~ setTimeout 2`);
+// });
 
-cuatro()
-  .then((data) => data.json())
-  .then((data) => console.log({data}))
-  .catch((error) => console.log({error}));
+// cuatro()
+//   .then((data) => data.json())
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }))
+//   .finally(() => console.log("final de la promesa"));
 
 tres();
+
+cinco();
