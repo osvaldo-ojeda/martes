91f9d81072421817da3faf441575c4736ea748bc
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-05-14 19:41:27 -0300

    clase6

--- express/index.js
+++ express/index.js
@@ -1,6 +1,15 @@
 import express from "express";
 import moment from "moment";
+import url from "url";
+import path from "path";
+// console.log(`🚀 ~ path:`, path)
+// console.log(`🚀 ~ url:`, url)
 // console.log(`🚀 ~ express:`, express)
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(`🚀 ~ meta.url:`, import.meta.url);
+console.log(`🚀 ~ __filename:`, __filename);
+console.log(`🚀 ~ __dirname:`, __dirname);
 
 const app = express();
 // console.log(`🚀 ~ app:`, app)
@@ -26,15 +35,16 @@ app.get("/count", (req, res) => {
   res.send({ count });
 });
 
-
-app.get("/archivo", (req, res) => {
-
-res.sendFile()
+app.get("/archivo/:file", (req, res) => {
+  // console.log(`🚀 ~ app.get ~ req:`, req.params)
+  // const file = req.params.file;
+  // const id = req.params.id;
+  // const numero = req.params.numero;
+  // const { file, id, numero } = req.params;
+  const { file } = req.params;
+  console.log(`🚀 ~ app.get ~ file:`, file);
+  res.sendFile(path.join(__dirname, "public", `${file}.html`));
 });
-
-
-
-
 
 app.listen(8080, () => {
   console.log(`server ok http://localhost:8080`);
