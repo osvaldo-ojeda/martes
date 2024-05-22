5d9d7d0771a47081d94908e777ac913aa51de942
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-05-21 22:07:57 -0300

    clase8

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -5,8 +5,35 @@ class UserControllers {
     try {
       const query = "SELECT id, name, mail FROM user";
       const [result] = await db.query(query);
-     //  console.log(`🚀 ~ UserControllers ~ getAllUser ~ data:`, result);
+      //  console.log(`🚀 ~ UserControllers ~ getAllUser ~ data:`, result);
       res.status(200).send({ success: true, message: result });
+    } catch (error) {
+      res.status(400).send({ success: false, message: error });
+    }
+  }
+
+  async createUser(req, res) {
+    try {
+      const { name, mail, password } = req.body;
+      const query = `INSERT INTO user (name, password, mail) VALUES(?,?,?)`;
+      const [result] = await db.query(query, [name, password, mail]);
+      res
+        .status(200)
+        .send({ success: true, message: "usuario creado con exito" });
+    } catch (error) {
+      res.status(400).send({ success: false, message: error });
+    }
+  }
+  async updateUser(req, res) {
+    try {
+      const { id } = req.params;
+      const { name, mail, password } = req.body;
+      const query = `UPDATE user SET name=?, password=?, mail=? WHERE id=?`;
+      const [result] = await db.query(query, [name, password, mail, id]);
+      console.log(`🚀 ~ UserControllers ~ updateUser ~ result:`, result);
+      res
+        .status(200)
+        .send({ success: true, message: "usuario modificado con exito" });
     } catch (error) {
       res.status(400).send({ success: false, message: error });
     }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,18 +5,9 @@ const userControllers = new UserControllers();
 const userRoutes = Router();
 
 userRoutes.get("/", userControllers.getAllUser);
-userRoutes.get("/es", (req, res, next) => {
-  res.send("get all users es");
-});
-userRoutes.get("/:id", (req, res, next) => {});
-userRoutes.post("/");
-
-userRoutes.put("/:id", (req, res) => {
-  res.send("update user by id");
-});
-
-userRoutes.delete("/:id", (req, res) => {
-  res.send("delete user by id");
-});
+userRoutes.get("/:id");
+userRoutes.post("/", userControllers.createUser);
+userRoutes.put("/:id", userControllers.updateUser);
+userRoutes.delete("/:id");
 
 export default userRoutes;
